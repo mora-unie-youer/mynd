cbccdcda2916061301df0c94f4b951b5e34a324d
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-07-18 17:32:35 +0000

    Implemented line extraction from string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn extract_line(input: &str) -> (&str, &str) {
+    match input.split_once('\n') {
+        Some(tuple) => tuple,
+        None => (input, ""),
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }

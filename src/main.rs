2c1d04e3f5aa7805e0e7e3f3ff1a432097a82efa
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-07-19 19:42:35 +0000

    Implemented heading line simple parsing

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,26 @@ fn extract_line(input: &str) -> (&str, &str) {
     }
 }
 
+fn parse_heading_level(input: &str) -> Option<(&str, usize, &str)> {
+    let (line, other) = extract_line(input);
+    if !line.starts_with('*') {
+        return None;
+    }
+
+    let (level, heading) = match line.split_once(' ') {
+        Some((level, heading)) => {
+            if level.chars().all(|c| c == '*') {
+                (level.len(), heading.trim_start())
+            } else {
+                return None;
+            }
+        }
+        None => return None,
+    };
+
+    Some((other, level, heading))
+}
+
 fn main() {
     println!("Hello, world!");
 }
